f6ce19bdd43988dbb63e7b19dea6442f595320b8
Eloy Vicente Cadena <L22270514@TUXTLA.TECNM.MX>
2026-05-20 00:47:45 -0600

    Integracion inicial Firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 }
 
 dependencies {
+    implementation("com.google.firebase:firebase-firestore:25.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
