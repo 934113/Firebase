d8253c0c2f427e23099e2d92b56f5ddc98445695
Sergio27mijares <sergiodejesusdominguezmijares@gmail.com>
2026-05-21 00:32:37 -0600

    Eliminar archivo Gasto.java obsoleto para limpiar el proyecto

--- app/src/main/java/com/example/firebase/Gasto.java
+++ app/src/main/java/com/example/firebase/Gasto.java
@@ -1,9 +1,10 @@
 package com.example.firebase;
 
 /**
- * @deprecated Use Gasto.kt instead.
- * Renamed to avoid collision with Gasto.kt
+ * @deprecated Este archivo ya no es necesario. Usa Gasto.kt.
+ * Se ha eliminado el modificador 'public' para evitar errores de compilación
+ * ya que el nombre de la clase no coincide con el del archivo.
  */
 @Deprecated
-public class GastoOld {
+class GastoOld {
 }
